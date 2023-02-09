17ee066afef7d4191a44e8a6c4a843add471c325
Olivér Falvai <oliver.falvai@bitrise.io>
2023-02-09 11:01:10 +0100

    Use ExitCode constants

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
 	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/bitrise-io/go-utils/v2/env"
+	. "github.com/bitrise-io/go-utils/v2/exitcode"
 	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-steplib/steps-restore-dart-cache/step"
 )
 
 func main() {
-	os.Exit(run())
+	exitCode := run()
+	os.Exit(int(exitCode))
 }
 
-func run() int {
+func run() ExitCode {
 	logger := log.NewLogger()
 	envRepo := env.NewRepository()
 	inputParser := stepconf.NewInputParser(envRepo)
@@ -23,8 +25,8 @@ func run() int {
 
 	if err := cacheStep.Run(); err != nil {
 		logger.Errorf(err.Error())
-		return 1
+		return Failure
 	}
 
-	return 0
+	return Success
 }
