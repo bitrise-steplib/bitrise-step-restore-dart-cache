6fa41b4a4723db98e052b25ec26754bf7a541af4
Olivér Falvai <oliver.falvai@bitrise.io>
2023-02-09 15:31:31 +0100

    Initial step version (#1)
    * Initial step version
    
    * Cleanup
    
    * Use ExitCode constants
    
    * Remove dot import

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"github.com/bitrise-io/go-steputils/v2/stepconf"
+	"github.com/bitrise-io/go-utils/v2/command"
+	"github.com/bitrise-io/go-utils/v2/env"
+	"github.com/bitrise-io/go-utils/v2/exitcode"
+	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-steplib/steps-restore-dart-cache/step"
+)
+
+func main() {
+	exitCode := run()
+	os.Exit(int(exitCode))
+}
+
+func run() exitcode.ExitCode {
+	logger := log.NewLogger()
+	envRepo := env.NewRepository()
+	inputParser := stepconf.NewInputParser(envRepo)
+	cmdFactory := command.NewFactory(envRepo)
+	cacheStep := step.New(logger, inputParser, envRepo, cmdFactory)
+
+	if err := cacheStep.Run(); err != nil {
+		logger.Errorf(err.Error())
+		return exitcode.Failure
+	}
+
+	return exitcode.Success
+}
